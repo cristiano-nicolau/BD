5d747d4fcef01d8a7e5c802f3d26604dfeefeb7d
Vasco-Faria <vascomfaria@ua.pt>
2023-05-31 23:13:52 +0100

    commit

--- Projeto_Final/YoutubeTables.sql
+++ Projeto_Final/YoutubeTables.sql
@@ -1,7 +1,7 @@
 	USE p5g2;
 	GO
 
-	/*CREATE SCHEMA Youtube;
+	CREATE SCHEMA Youtube;
 	GO
 
 	CREATE TABLE Youtube.Estados (
@@ -107,6 +107,8 @@ CREATE TABLE Youtube.Comentários (
 	);
 	GO
 
+	/*
+
 	ALTER TABLE Youtube.Comentários ADD CONSTRAINT CódigoV FOREIGN KEY (CódigoV) REFERENCES Youtube.Conteúdo(Codigo);
 
 
@@ -134,128 +136,7 @@ CREATE TABLE Youtube.Comentários (
 	GO
 
 
-
-
-CREATE TRIGGER trg_UpdateLikes
-ON Youtube.Conteúdo
-AFTER INSERT
-AS
-BEGIN
-    UPDATE c
-    SET c.Num_Likes = c.Num_Likes + 1
-    FROM Youtube.Conteúdo c
-    INNER JOIN inserted i ON c.Codigo = i.Codigo;
-END;
-
-
-ALTER TRIGGER [Youtube].[trg_UpdateLikes]
-ON [Youtube].[Conteúdo]
-AFTER INSERT
-AS
-BEGIN
-    DECLARE @code INT;
-    SET @code = (SELECT Codigo FROM inserted);
-    
-    UPDATE c
-    SET c.Num_Likes = c.Num_Likes + 1
-    FROM Youtube.Conteúdo c
-    WHERE c.Codigo = @code;
-END;
-
-
-CREATE FUNCTION Youtube.CalculatePlaylistDuration
-(
-    @PlaylistID INT
-)
-RETURNS TIME
-AS
-BEGIN
-    DECLARE @TotalSeconds INT;
-
-    SELECT @TotalSeconds = SUM(DATEPART(SECOND, c.Duracao)) +
-                           SUM(DATEPART(MINUTE, c.Duracao)) * 60 +
-                           SUM(DATEPART(HOUR, c.Duracao)) * 3600
-    FROM Youtube.Conteúdo c
-    INNER JOIN Youtube.PlaylistVideo pv ON c.Codigo = pv.VideoID
-    WHERE pv.PlaylistID = @PlaylistID;
-
-    DECLARE @TotalDuration TIME;
-    SET @TotalDuration = DATEADD(SECOND, @TotalSeconds, '00:00:00');
-
-    RETURN @TotalDuration;
-END;
-
-
-
-CREATE PROCEDURE Youtube.UpdateSubscribers
-    @CanalName VARCHAR(20)
-AS
-BEGIN
-    UPDATE Youtube.Canal
-    SET Num_Subscritores = Num_Subscritores + 1
-    WHERE Nome_Utilizador = @CanalName;
-END;
-
-
-
-CREATE PROCEDURE Youtube.UpdateSubscribers2
-    @CanalName VARCHAR(20)
-AS
-BEGIN
-    UPDATE Youtube.Canal
-    SET Num_Subscritores = Num_Subscritores - 1
-    WHERE Nome_Utilizador = @CanalName;
-END;
-
-
-
-CREATE PROCEDURE [Youtube].[CreateUtilizadorAndCanal]
-    @Nome_Utilizador VARCHAR(20),
-    @Email VARCHAR(20),
-    @Senha VARCHAR(15),
-    @Nome VARCHAR(20),
-    @Nascimento DATE
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    -- Insert into Utilizador table
-    INSERT INTO [Youtube].[Utilizador] (Nome_Utilizador, Email, Senha, Nome, Data_de_Nascimento)
-    VALUES (@Nome_Utilizador, @Email, @Senha, @Nome, @Nascimento);
-
-    -- Insert into Canal table
-    INSERT INTO [Youtube].[Canal] (Nome_Utilizador, Num_Subscritores, Num_Conteudo, Descrição_Canal)
-    VALUES (@Nome_Utilizador, 0, 0, '');
-END;
-
-
-
-
-CREATE PROCEDURE Youtube.DeletePlaylist
-    @PlaylistID INT
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    BEGIN TRANSACTION;
-
-    BEGIN TRY
-        -- Delete corresponding records from PlaylistVideo table
-        DELETE FROM [Youtube].[PlaylistVideo] WHERE PlaylistID = @PlaylistID;
-
-        -- Delete the playlist
-        DELETE FROM [Youtube].[Playlist] WHERE CodigoP = @PlaylistID;
-
-        COMMIT TRANSACTION;
-    END TRY
-    BEGIN CATCH
-        ROLLBACK TRANSACTION;
-        THROW; -- Rethrow the error
-    END CATCH;
-END;
-
-
-*/
+	*/
 
 
 
