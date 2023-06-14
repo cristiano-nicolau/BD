cdac038fae8d2177d669ea0d1c1067da29d1c6dc
Cristiano Nicolau <115578289+cristiano-nicolau@users.noreply.github.com>
2023-06-14 15:33:53 +0100

    Delete Triggers,UDFs,Procedures directory

--- Triggers,UDFs,Procedures/Index_Data_Visualizacao.sql
+++
@@ -1,1 +0,0 @@
-CREATE INDEX idx_Data_de_Visualizacao ON Youtube.Histórico (Data_de_Visualizacao);
--- Triggers,UDFs,Procedures/Index_Nome_Utilizador.sql
+++
@@ -1,7 +0,0 @@
-
-
-USE p5g2;
-go
-
-CREATE INDEX idx_Nome_Utilizador ON Youtube.Utilizador (Nome_Utilizador);
-
